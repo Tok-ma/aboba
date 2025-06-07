2197d42c0e5b14138c35c5802c29788cb4be5a17
IgumnovDaniil <igumnov.danechka@mail.ru>
2025-06-07 12:28:34 +0300

    Bobik

--- Практическая №5/7/app.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-const PrintHello = () => {
-    return <h1>I Love React</h1>;
-}
-
-ReactDOM.render(<PrintHello />, document.querySelector("#myDiv"));
